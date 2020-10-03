450f024ca3ec78ae045de2a30cccbe11385590ec
Hmiya <h.miya.work@gmail.com>
2020-10-03 11:51:45 +0900

    memo and sample codes of sync.Pool

---
+++ sync/08/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	pool := &sync.Pool{
+		New: func() interface{} {
+			fmt.Println("Creating new instance.")
+			return struct{}{}
+		},
+	}
+
+	pool.Get()
+	instance := pool.Get()
+	pool.Put(instance)
+	pool.Get()
+}
